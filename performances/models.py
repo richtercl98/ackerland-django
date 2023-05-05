73779c753506a4e9d601937296ff200bdb861307
richtercl98 <clemensrichter64@gmx.de>
2023-05-05 16:18:05 +0200

    final fixes???

--- performances/models.py
+++ performances/models.py
@@ -12,7 +12,7 @@ class Act(models.Model):
     image = models.ImageField(upload_to='static/img/acts/')
     sound_sample_url = models.URLField(max_length=256, blank=True)
     insta_url = models.URLField(max_length=256, blank=True)
-    corresponding_user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    corresponding_user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, unique=False)
 
     def __str__(self):
         return self.stage_name
@@ -22,7 +22,7 @@ class Workshop(models.Model):
     description = models.CharField(max_length=300)
     max_participants = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    corresponding_user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    corresponding_user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, unique=False)
     image = models.ImageField(upload_to='static/img/workshops/', default=None)
 
     def __str__(self):
