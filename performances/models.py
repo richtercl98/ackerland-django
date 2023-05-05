744ac9dea9d5141e60ee222659128a3217ca61b0
richtercl98 <clemensrichter64@gmx.de>
2023-05-05 18:08:37 +0200

    make image for acts/workshops optional

--- performances/models.py
+++ performances/models.py
@@ -9,7 +9,7 @@ class Act(models.Model):
     stage_name = models.CharField(max_length=64)
     description = models.CharField(max_length=400)
     genre = models.CharField(max_length=64, blank=True)
-    image = models.ImageField(upload_to='static/img/acts/')
+    image = models.ImageField(upload_to='static/img/acts/', null=True, blank=True)
     sound_sample_url = models.URLField(max_length=256, blank=True)
     insta_url = models.URLField(max_length=256, blank=True)
     corresponding_users = models.ManyToManyField(User)
@@ -23,7 +23,7 @@ class Workshop(models.Model):
     max_participants = models.IntegerField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     corresponding_users = models.ManyToManyField(User)
-    image = models.ImageField(upload_to='static/img/workshops/', default=None)
+    image = models.ImageField(upload_to='static/img/workshops/', null=True, blank=True, default=None)
 
     def __str__(self):
         return self.name
